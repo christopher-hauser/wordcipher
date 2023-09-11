b00deb068a33021a3ec90673e88c8d72419db97e
christopher-hauser <chauser6@gmail.com>
2023-09-11 11:43:15 -0400

    Add production schema creation to env.py

--- migrations/env.py
+++ migrations/env.py
@@ -89,8 +89,13 @@ def run_migrations_online():
             process_revision_directives=process_revision_directives,
             **current_app.extensions['migrate'].configure_args
         )
+        # Create a schema (only in production)
+        if environment == "production":
+            connection.execute(f"CREATE SCHEMA IF NOT EXISTS {SCHEMA}")
 
         with context.begin_transaction():
+            if environment == "production":
+                context.execute(f"SET search_path TO {SCHEMA}")
             context.run_migrations()
 
 
